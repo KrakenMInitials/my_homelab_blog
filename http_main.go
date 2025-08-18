66878c91f08782579c13272876501f0e5c7bcffc
KrakenM (T) <moemyatag05@gmail.com>
2025-08-18 02:56:07 -0700

    Add Mux

--- http_main.go
+++ http_main.go
@@ -9,32 +9,41 @@ type api struct {
 	addr string
 }
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World! -server "))
-	switch r.Method {
-	case http.MethodGet:
-		switch r.URL.Path {
-		case "/":
-			w.Write([]byte("This is the index page."))
-		case "/users":
-			w.Write([]byte("This is the users page."))
+// func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("Hello World! -server "))
+// 	switch r.Method {
+// 	case http.MethodGet:
+// 		switch r.URL.Path {
+// 		case "/":
+// 			w.Write([]byte("This is the index page."))
+// 		case "/users":
+// 			w.Write([]byte("This is the users page."))
 
-		}
-	default:
-		w.Write([]byte("404 error unknown"))
-	}
+// 		}
+// 	default:
+// 		w.Write([]byte("404 error unknown"))
+// 	}
 
-}
+// }
 
 func main() {
 	api := &api{addr: ":8080"}
 	
-	svr := &http.Server{
+	mux := http.NewServeMux()
+
+	srv := &http.Server{
 		Addr: api.addr,
-		Handler: api,
+		Handler: mux,
 	}
-	err := svr.ListenAndServe()
+	
+
+
+	mux.HandleFunc("GET /users", api.getUsersHandler)
+	mux.HandleFunc("POST /users", api.createUsersHandler)
+	
+	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
